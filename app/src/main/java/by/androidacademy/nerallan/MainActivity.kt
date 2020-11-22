e3008edef63f95b50f7da8ead86674e79d3572b0
Maksim Budzko <MBudko@alfabank.ru>
2020-11-23 00:53:13 +0300

    update deps versions, add useful ext's, launch empty movie detail activity

--- app/src/main/java/by/androidacademy/nerallan/MainActivity.kt
+++ app/src/main/java/by/androidacademy/nerallan/MainActivity.kt
@@ -1,11 +1,23 @@
 package by.androidacademy.nerallan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import by.androidacademy.nerallan.ext.bind
+import by.androidacademy.nerallan.ext.launchActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val greetingTextView by bind<TextView>(R.id.main_screen_greeting_message)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingTextView.setOnClickListener { moveToMovieDetailsScreen() }
+    }
+
+    private fun moveToMovieDetailsScreen() {
+        launchActivity<MovieDetailsActivity> { }
     }
 }
